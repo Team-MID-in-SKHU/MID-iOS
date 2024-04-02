807d9f1bc60bbf655a079bd5051da272c24460f3
Cheon, Seongwoo <cjs1399@naver.com>
2024-04-02 09:29:01 +0900

    [Setting] #1 - Remove storyBoard

---
+++ MID/MID/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MID
+//
+//  Created by 천성우 on 4/2/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+extension ViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        view.backgroundColor = .blue
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}
