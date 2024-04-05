7b4bda6d63af37a99e85ab419fda81574c64aeff
Cheon, Seongwoo <cjs1399@naver.com>
2024-04-05 12:54:26 +0900

    [Feature] #3 - edit UserInfo View

--- MID/MID/Presentation/MyPage/ViewControllers/MyPageViewController.swift
+++ MID/MID/Presentation/MyPage/ViewControllers/MyPageViewController.swift
@@ -5,4 +5,35 @@
 //  Created by 천성우 on 4/5/24.
 //
 
-import Foundation
+import UIKit
+
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class MyPageViewController: BaseViewController {
+    
+    
+    private let viewModel = OnboardingViewModel()
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    
+   
+    // MARK: - Properties
+    
+    override func bindViewModel() {
+
+    }
+    
+    override func setStyles() {
+       
+    }
+    
+    override func setLayout() {
+        
+    }
+    
+    
+}
--- MID/MID/Presentation/MyPage/Views/UserInfoView.swift
+++ MID/MID/Presentation/MyPage/Views/UserInfoView.swift
@@ -5,4 +5,47 @@
 //  Created by 천성우 on 4/5/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class UserInfoView: BaseView {
+    
+    // MARK: - UI Components
+    
+    private var userImage = UIImageView()
+    private var userName = UILabel()
+    
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    // MARK: - UI Components Property
+    
+    override func setStyles() {
+        userImage.do {
+            $0.image = ImageLiterals.MyPage.userImage
+        }
+        
+        userName.do {
+            $0.text = "MID님의 마이페이지"
+            $0.font = .systemFont(ofSize: 24)
+        }
+    }
+    
+    // MARK: - Layout Helper
+    
+    override func setLayout() {
+        
+    }
+    
+    // MARK: - @objc Methods
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
