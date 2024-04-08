32ed22cc3035431fe956cc0857ad31597fad1d81
Cheon, Seongwoo <cjs1399@naver.com>
2024-04-08 11:55:48 +0900

    [Feature] #5 - edit TodoViewController

--- MID/MID/Presentation/Todo/ViewControllers/TodoViewController.swift
+++ MID/MID/Presentation/Todo/ViewControllers/TodoViewController.swift
@@ -13,9 +13,10 @@ import RxSwift
 import RxCocoa
 
 final class TodoViewController: BaseViewController {
+
+    private let titleView = LookUpButtonView()
     
-    
-    private let viewModel = OnboardingViewModel()
+    private let viewModel = TodoViewModel()
     private let disposeBag = DisposeBag()
     
     // MARK: - UI Components
@@ -24,17 +25,27 @@ final class TodoViewController: BaseViewController {
     // MARK: - Properties
     
     override func bindViewModel() {
-
+        titleView.buttonFrame.rx.tap
+            .bind { [weak self] in
+                guard let self else { return }
+                self.viewModel.inputs.lookUpButtonTap()
+            }
+            .disposed(by: disposeBag)
     }
     
     override func setStyles() {
-       
+        view.backgroundColor = .gray600
     }
     
     override func setLayout() {
+        view.addSubviews(titleView)
         
+        titleView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(SizeLiterals.Screen.screenHeight * 0.01355)
+            $0.leading.equalToSuperview()
+            $0.width.equalTo(SizeLiterals.Screen.screenWidth * 0.888)
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 0.117)
+        }
     }
-    
-    
 }
 
