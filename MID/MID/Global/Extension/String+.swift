be0883189b7f86c3a17d557560ac958732f0f227
Cheon, Seongwoo <cjs1399@naver.com>
2024-04-12 16:17:02 +0900

    [Feature] #5 - KoreanFormattedData implement

--- MID/MID/Global/Extension/String+.swift
+++ MID/MID/Global/Extension/String+.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension String {
+    static func koreanFormattedDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "M월 d일 EEEE"
+        return dateFormatter.string(from: Date())
+    }
+}
