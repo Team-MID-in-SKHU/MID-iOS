7718a7e9001c38a4d620d46627ddb562f984cec9
Cheon, Seongwoo <cjs1399@naver.com>
2024-04-19 11:45:51 +0900

    [Feature] #7 - Create CalendarView CollectionViewCell

--- MID/MID/Presentation/MonthTodo/Views/CalendarView.swift
+++ MID/MID/Presentation/MonthTodo/Views/CalendarView.swift
@@ -9,16 +9,11 @@ import UIKit
 
 import SnapKit
 import Then
-import RxSwift
-import RxCocoa
 
 final class CalendarView: BaseView {
     
     // MARK: - UI Components
     
-    private let titleLabel = UILabel()
-    let calendarView = UICalendarView()
-    ㅣㄷㅅ 
     
     // MARK: - UI Components Property
     
@@ -34,22 +29,6 @@ final class CalendarView: BaseView {
         backgroundColor = .gray600
         layer.cornerRadius = 10
         
-        titleLabel.do {
-            $0.text = "이달의 일정"
-            $0.font = .fontGuide(.head1)
-            $0.textColor = .white000
-        }
-        
-        calendarView.do {
-            $0.calendar = .current
-            $0.locale = .current
-            $0.fontDesign = .rounded
-            $0.backgroundColor = .gray600
-            $0.tintColor = .red500
-        }
-   
-        
-
 
     }
     
@@ -57,17 +36,13 @@ final class CalendarView: BaseView {
     // MARK: - Layout Helper
     
     override func setLayout() {
-        addSubviews(titleLabel, calendarView)
-        
-        titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(SizeLiterals.Screen.screenHeight * 11 / 812)
-            $0.leading.equalToSuperview().offset(SizeLiterals.Screen.screenWidth * 21 / 375)
-        }
-        
-        calendarView.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(SizeLiterals.Screen.screenHeight * 29 / 812)
-            $0.leading.trailing.equalToSuperview()
-        }
+//        addSubviews(titleLabel)
+//        
+//        titleLabel.snp.makeConstraints {
+//            $0.top.equalToSuperview().offset(SizeLiterals.Screen.screenHeight * 11 / 812)
+//            $0.leading.equalToSuperview().offset(SizeLiterals.Screen.screenWidth * 21 / 375)
+//        }
+// 
     }
     
     // MARK: - Methods
---
+++ MID/MID/Presentation/MonthTodo/Views/Components/MonthTodoCalendarCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  MonthTodoCalendarCollectionViewCell.swift
+//  MID
+//
+//  Created by 천성우 on 4/19/24.
+//
+
+
+import UIKit
+
+final class CalendarCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "CalendarCollectionViewCell"
+    
+    private lazy var dayLabel = UILabel()
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configure()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
+    
+//    override func prepareForReuse() {
+//        self.dayLabel.text = nil
+//    }
+    
+    func update(day: String) {
+        self.dayLabel.text = day
+    }
+    
+    private func configure() {
+        self.addSubview(self.dayLabel)
+        self.dayLabel.font = .boldSystemFont(ofSize: 12)
+        self.dayLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.dayLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
+            self.dayLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+    }
+    
+}
