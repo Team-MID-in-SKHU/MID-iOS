3ca4ca5339f836e369db7f29706915607bc08d82
Cheon, Seongwoo <cjs1399@naver.com>
2024-05-15 15:45:10 +0900

    [Feature] #11 - add SignUp VC with func

---
+++ MID/MID/Presentation/Login/ViewControllers/SignUpPageThirdViewController.swift
@@ -0,0 +1,76 @@
+//
+//  SignUpPageThirdViewController.swift
+//  MID
+//
+//  Created by 천성우 on 5/15/24.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class SignUpPageThirdViewController: BaseViewController {
+    
+    
+    private let viewModel = TodoViewModel()
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    
+    private lazy var scrollView = UIScrollView()
+    private let signUpThirdPage = SignUpThirdPageView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Properties
+    
+    
+    override func bindViewModel() {
+        signUpThirdPage.nextButton.rx.tap
+            .bind { [weak self] in
+                guard let self else { return }
+                self.signUpSuccpuess()
+            }
+            .disposed(by: disposeBag)
+
+    }
+    
+    override func setStyles() {
+        view.backgroundColor = .gray600
+        
+        scrollView.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.backgroundColor = .clear
+        }
+    }
+    
+    override func setLayout() {
+        view.addSubviews(signUpThirdPage)
+        
+        signUpThirdPage.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func signUpSuccpuess() {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let sceneDelegate = windowScene.delegate as? SceneDelegate,
+           let window = sceneDelegate.window {
+            let vc = LoginViewController()
+            let rootVC = UINavigationController(rootViewController: vc)
+            rootVC.navigationController?.isNavigationBarHidden = true
+            window.rootViewController = rootVC
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
+
