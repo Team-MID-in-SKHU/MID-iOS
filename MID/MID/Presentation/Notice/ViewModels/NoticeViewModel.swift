1a1c8d4effcc93b9eb82336470a18a52e30d9c96
Cheon, Seongwoo <cjs1399@naver.com>
2024-05-16 14:08:09 +0900

    [Fix] #11 - edit NoticeViewModel

--- MID/MID/Presentation/Notice/ViewModels/NoticeViewModel.swift
+++ MID/MID/Presentation/Notice/ViewModels/NoticeViewModel.swift
@@ -5,4 +5,34 @@
 //  Created by 천성우 on 4/5/24.
 //
 
-import Foundation
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+protocol NoticeViewModelInput {
+    func myInformationDidTap()
+    func interestModifyDidTap()
+    func pushAlarmDidTap()
+    func logOutDidTap()
+    func withdrawalDidTap()
+}
+
+protocol NoticeViewModelOutput {
+}
+
+protocol NoticeViewModelType {
+    var inputs: NoticeViewModelInput { get }
+    var outputs: NoticeViewModelOutput { get }
+}
+
+final class NoticeViewModel: NoticeViewModelInput, NoticeViewModelOutput, NoticeViewModelType {
+    
+    var inputs: MyPageViewModelInput { return self }
+    var outputs: MyPageViewModelOutput { return self }
+    
+    init() {
+        
+    }
+    
+}
