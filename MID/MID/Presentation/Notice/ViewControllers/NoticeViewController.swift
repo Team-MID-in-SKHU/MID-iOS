f4deeaa3cfb0c7d52fff3be755758f7c28e3b838
Cheon, Seongwoo <cjs1399@naver.com>
2024-05-16 14:09:52 +0900

    [Feature] #11 - add Notice VC

--- MID/MID/Presentation/Notice/ViewControllers/NoticeViewController.swift
+++ MID/MID/Presentation/Notice/ViewControllers/NoticeViewController.swift
@@ -5,4 +5,35 @@
 //  Created by 천성우 on 4/5/24.
 //
 
-import Foundation
+import UIKit
+
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class NoticeViewController: BaseViewController {
+    
+    
+    private let viewModel = NoticeViewModel()
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    
+    override func viewDidLoad() {
+
+        
+    }
+    
+    // MARK: - Properties
+    
+    override func bindViewModel() {}
+    
+    override func setStyles() {}
+    
+    override func setLayout() {}
+    
+    override func setRegister() {}
+    
+
+}
