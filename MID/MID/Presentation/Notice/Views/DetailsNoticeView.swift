3f2bda0a3f9b7460340837eddb50f8d0aab2c29c
Cheon, Seongwoo <cjs1399@naver.com>
2024-05-31 12:08:39 +0900

    [Feature] #13 - add File

---
+++ MID/MID/Presentation/Notice/Views/DetailsNoticeView.swift
@@ -0,0 +1,37 @@
+//
+//  DetailsNoticeView.swift
+//  MID
+//
+//  Created by 천성우 on 5/24/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class DetailsNoticeView: BaseView {
+    
+    // MARK: - UI Components
+
+
+    // MARK: - Initializer
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    // MARK: - UI Components Property
+    
+    override func setStyles() {}
+    
+    // MARK: - Layout Helper
+    
+    override func setLayout() {}
+    
+    // MARK: - @objc Methods
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
