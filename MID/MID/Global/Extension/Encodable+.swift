7179bac2f72581d597f0c34904d0cc7c1e4b8a52
Cheon, Seongwoo <cjs1399@naver.com>
2024-06-04 09:50:17 +0900

    [Feature] #13 - add encodable Extension

---
+++ MID/MID/Global/Extension/Encodable+.swift
@@ -0,0 +1,23 @@
+//
+//  Encodable+.swift
+//  MID
+//
+//  Created by 천성우 on 6/4/24.
+//
+
+import Foundation
+
+// MARK: - Encodable Extension
+
+extension Encodable {
+    
+  func asParameter() throws -> [String: Any] {
+    let data = try JSONEncoder().encode(self)
+    guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            as? [String: Any] else {
+      throw NSError()
+    }
+    
+    return dictionary
+  }
+}
