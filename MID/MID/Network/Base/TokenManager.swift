1648e959b87c139ae919c38a9738887a37059a0a
Cheon, Seongwoo <cjs1399@naver.com>
2024-06-04 09:48:37 +0900

    [Setting] #13 - add Token Manager

---
+++ MID/MID/Network/Base/TokenManager.swift
@@ -0,0 +1,69 @@
+//
+//  TokenManager.swift
+//  MID
+//
+//  Created by 천성우 on 6/4/24.
+//
+
+import UIKit
+import Security
+
+import Moya
+import RxSwift
+import RxMoya
+
+final class TokenManager {
+    
+    static let shared = TokenManager()
+    private let authProvider = MoyaProvider<AuthTarget>(plugins: [NetworkLoggerPlugin()])
+    
+    private init() {}
+    
+    func refreshNewToken(retryHandler: @escaping (_ success: Bool) -> Void) {
+        AuthAPI.shared.getRefreshToken { response in
+            guard response != nil else {
+                retryHandler(false)
+                return
+            }
+            guard let data = response?.data else { return }
+            APIConstants.jwtToken = data.accessToken
+            APIConstants.refreshToken = data.refreshToken
+            if let accessTokenData = data.accessToken.data(using: .utf8) {
+                KeychainHelper.save(key: I18N.Auth.jwtToken, data: accessTokenData)
+            }
+
+            if let refreshTokenData = data.refreshToken.data(using: .utf8) {
+                KeychainHelper.save(key: I18N.Auth.refreshToken, data: refreshTokenData)
+            }
+            retryHandler(true)
+        }
+    }
+    
+    func refreshNewToken() -> Observable<Void> {
+        return authProvider.rx.request(.tokenRefresh)
+            .asObservable()
+            .do(onNext: { response in
+                if response.statusCode == 401 {
+                    let mainViewController = LoginViewController()
+                    guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else { return }
+                    sceneDelegate.window?.rootViewController = UINavigationController(rootViewController: mainViewController)
+                    sceneDelegate.window?.makeKeyAndVisible()
+                }
+            })
+            .decode(decodeType: RefreshTokenResponseDto.self)
+            .do(onNext: { data in
+                print("🔥")
+                print("New Token is now valid.")
+                APIConstants.jwtToken = data.accessToken
+                APIConstants.refreshToken = data.refreshToken
+                if let accessTokenData = data.accessToken.data(using: .utf8) {
+                    KeychainHelper.save(key: I18N.Auth.jwtToken, data: accessTokenData)
+                }
+
+                if let refreshTokenData = data.refreshToken.data(using: .utf8) {
+                    KeychainHelper.save(key: I18N.Auth.refreshToken, data: refreshTokenData)
+                }
+            })
+            .map {_ in}
+    }
+}
