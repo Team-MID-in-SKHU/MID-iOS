12457befcf1afe4742baac3220eab997ff67608f
Cheon, Seongwoo <cjs1399@naver.com>
2024-06-10 22:42:46 +0900

    [Feature] #13 - add Auth DTO, with signUp & Login

---
+++ MID/MID/Data/Auth/AuthDTO.swift
@@ -0,0 +1,25 @@
+//
+//  AuthDTO.swift
+//  MID
+//
+//  Created by 천성우 on 6/10/24.
+//
+
+import Foundation
+
+
+struct SignUpRequestBody: Codable {
+    let studentNo, password, name, department: String
+    let phoneNumber, fcmToken, roleType: String
+}
+
+
+struct SignUpResponseBody: Codable {}
+
+
+struct LoginRequestBody: Codable {
+    let studentNo, password, fcmToken: String
+}
+
+
+struct LoginResponseBody: Codable {}
