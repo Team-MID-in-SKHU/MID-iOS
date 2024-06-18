1f75050bc30194d9a4e1ca50a56a20769daa6faf
Cheon, Seongwoo <cjs1399@naver.com>
2024-06-18 23:45:35 +0900

    [Feature] #15 - 푸쉬알림 설정

--- MID/MID/Application/AppDelegate.swift
+++ MID/MID/Application/AppDelegate.swift
@@ -6,25 +6,33 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+       requestAuthorizationForRemotePushNotification()
+       return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
-
-
+    
+    // 푸시 알림 권한 요청 메서드
+    func requestAuthorizationForRemotePushNotification() {
+        let current = UNUserNotificationCenter.current()
+        current.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            // granted가 true로 떨어지면 푸시를 받을 수 있습닏.
+        }
+    }
 }
 
